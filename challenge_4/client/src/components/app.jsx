b7504ede47a1295eecaf8dfb653388aa858f2086
Ricardo Figueroa <rfigueror@gmail.com>
2018-07-12 17:29:48 -0500

    rendering of table

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -1,9 +1,34 @@
 var React = require('react');
 var reactdom = require('react-dom');
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  // Don't call this.setState() here!
+    this.state = {
+      round: 0,
+      gameState: {}
+    };
+    
+  }
+
   render() {
-    return <h1>Hello</h1>;
+    return (
+        <table>
+          <thead>
+            <tr>
+              <th colspan="2">The table header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The table body</td>
+              <td>with two columns</td>
+            </tr>
+          </tbody>
+        </table>
+    );
   }
 }
 
